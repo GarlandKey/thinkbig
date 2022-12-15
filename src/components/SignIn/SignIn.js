2827c0f9fc93554788251df96f75f9dae9e18641
Garland Key <hello@garlandkey.com>
2022-12-15 17:20:14 -0500

    Added boilerplate files for App, SignIn and TopNav components

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -1,25 +1,11 @@
-import logo from '../../logo.svg';
-import './App.css';
+import './SignIn.css';
 
-function App() {
+function SignIn() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="SignIn">
+      
     </div>
   );
 }
 
-export default App;
+export default SignIn;
